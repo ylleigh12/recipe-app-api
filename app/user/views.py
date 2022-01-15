8cb71689b48b5fef475a67ef3368f9d69e43264d
course <ylleigh12@gmail.com>
2022-01-15 13:18:10 +0000

    Added manage user endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -19,8 +19,8 @@ class CreateTokenView(ObtainAuthToken):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
-    authentication_class = (authentication.TokenAuthentication,)
-    permission_class = (permissions.IsAuthenticated,)
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_object(self):
         """Retrieve and return authentication user"""
